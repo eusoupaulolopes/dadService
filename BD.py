ee6674a894cf05aa91356502c60393698c020cba
Allan Gonçalves <allangoncalves@outlook.com>
2018-06-08 18:47:03 -0300

    updating BD.py

--- BD.py
+++ BD.py
@@ -3,12 +3,14 @@
 
 # In[1]:
 
+
 import pandas as pd
 import unicodedata
 import datetime
 
 
 # In[2]:
+
 
 url_componentes = 'componentes-graduacao.csv'
 url_unidades = 'http://dados.ufrn.br/dataset/da6451a5-1a59-4630-bdc2-97f6be4a59c2/resource/3f2e4e32-ef1a-4396-8037-cbc22a89d97f/download/unidades.csv'
@@ -17,6 +19,7 @@ url_avaliacoes = 'http://dados.ufrn.br/dataset/d5723d75-7e6e-4264-82aa-b96909b69
 
 
 # In[3]:
+
 
 df_unidades = pd.read_csv(url_unidades, ';')
 df_unidades = df_unidades[['id_unidade_responsavel', 'unidade_responsavel']]
@@ -28,16 +31,19 @@ df_unidades.head()
 
 # In[4]:
 
+
 df_docentes = pd.read_csv(url_docentes, ';')
 df_docentes.columns
 
 
 # In[5]:
 
+
 df_docentes = df_docentes[df_docentes['categoria'] == 'PROFESSOR DO MAGISTERIO SUPERIOR']
 
 
 # In[6]:
+
 
 df_docentes = df_docentes[['id_servidor', 'nome', 'formacao', 'id_unidade_lotacao', 'admissao']]
 df_docentes.rename(columns={'id_servidor': 'id_docente', 'admissao': 'data_admissao', 'id_unidade_lotacao': 'id_unidade'}, inplace=True)
@@ -48,12 +54,14 @@ df_docentes['id_unidade'].value_counts()
 
 # In[7]:
 
+
 df_componentes = pd.read_csv(url_componentes, sep=',')
 df_componentes = df_componentes[(df_componentes['unidade_responsavel'] == 'DEPARTAMENTO DE INFORMÁTICA E MATEMÁTICA APLICADA') | (df_componentes['unidade_responsavel'] == 'INSTITUTO METROPOLE DIGITAL')]
 df_componentes.head()
 
 
 # In[8]:
+
 
 unidades_normalizadas = [unicodedata.normalize('NFKD', unicode(nome, "utf-8")).encode('ASCII', 'ignore') for nome in df_componentes['unidade_responsavel']]
 series = pd.Series(data=unidades_normalizadas, index=df_componentes.index)
@@ -62,10 +70,12 @@ df_componentes['unidade_responsavel'] = series
 
 # In[9]:
 
+
 df_componentes = df_componentes.merge(df_unidades[['id_unidade', 'lotacao']], left_on='unidade_responsavel', right_on='lotacao', how='inner')
 
 
 # In[10]:
+
 
 df_componente_curricular = df_componentes[['id_componente', 'codigo']]
 df_componente_curricular.head()
@@ -73,11 +83,13 @@ df_componente_curricular.head()
 
 # In[11]:
 
+
 df_componente_lotacao = df_componentes[['codigo', 'id_unidade', 'nome']]
 df_componente_lotacao.head()
 
 
 # In[12]:
+
 
 turma_periodo = ['http://dados.ufrn.br/dataset/1938623d-fb07-41a4-a55a-1691f7c3b8b5/resource/5e77d066-d506-45eb-a21e-76aa79616fef/download/turmas-2017.1',
 'http://dados.ufrn.br/dataset/1938623d-fb07-41a4-a55a-1691f7c3b8b5/resource/01fe7343-fdf0-4a67-b915-2386b7c2fecb/download/turmas-2017.2',
@@ -93,15 +105,18 @@ turma_periodo = ['http://dados.ufrn.br/dataset/1938623d-fb07-41a4-a55a-1691f7c3b
 
 # In[13]:
 
+
 frames = [pd.read_csv(periodo, sep=';', usecols=['id_turma', 'ano', 'nivel_ensino', 'periodo', 'id_componente_curricular']) for periodo in turma_periodo]
 
 
 # In[14]:
 
+
 df_turmas = pd.concat(frames)
 
 
 # In[15]:
+
 
 df_turmas.rename(columns={'nivel_ensino': 'nivel', 'id_componente_curricular': 'id_componente'}, inplace=True)
 df_turmas = df_turmas[df_turmas['nivel'] == 'GRADUAÇÃO']
@@ -111,15 +126,18 @@ df_turmas = df_turmas.merge(df_componente_curricular[['id_componente']], on='id_
 df_turmas.head()
 
 
-# In[17]:
+# In[16]:
+
 
 df_avaliacoes = pd.read_csv(url_avaliacoes, sep=';', usecols=['id_docente', 'id_turma', 'qtd_discentes', 'postura_profissional_media', 'postura_profissional_DP', 'atuacao_profissional_media', 'atuacao_profissional_DP'])
 df_avaliacoes = df_avaliacoes.merge(df_turmas[['id_turma']].astype(int), on='id_turma', how='inner')
+df_avaliacoes = df_avaliacoes.merge(df_docentes[['id_docente']].astype(int), on='id_docente', how='inner')
 df_avaliacoes.rename(columns={'postura_profissional_media': 'postura_profissional', 'atuacao_profissional_media': 'atuacao_profissional'}, inplace=True)
 df_avaliacoes.head()
 
 
-# In[ ]:
+# In[17]:
+
 
 df_avaliacoes.to_csv('avaliacao.csv', sep=';', index=False)
 df_componente_lotacao.to_csv('componente_lotacao.csv', sep=';', index=False)
